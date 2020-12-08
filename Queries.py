00f994f4689f73706d7b3f3459e171c595750519
talhaunal <t.unal@etu.edu.tr>
2020-12-08 16:07:21 +0300

    Bugfix.

--- Queries.py
+++ Queries.py
@@ -146,7 +146,7 @@ def employee_page():
             if emplog:  # if a user is found, we want to redirect back to signup page so user can try again
                 emp = Employee.query.filter_by(memberID=emplog.employeeID).first()
                 if type == "carrier":
-                    response = make_response(render_template('carrierpage.html', employee=emp, employeelogin=emplog))
+                    response = make_response(render_template('carrier_page.html', employee=emp, employeelogin=emplog))
                     response.set_cookie("emp_id", str(emp.memberID))
                     response.set_cookie("emplog_id", str(emplog.employeeID))
                     response.set_cookie("emplog_type", str(emplog.type))
